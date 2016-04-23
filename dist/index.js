4bfa94475d4762abc496528195c078c060794f7c
Luc M <luc.momal@gmail.com>
2016-04-23 14:48:31 +0200

    v0.0.2

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = function (awesome) {
+  return 'everything is ' + awesome;
+};
+
+module.exports = exports['default'];
