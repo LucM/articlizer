2ab828ed5f51fd9f8d4f11c8c1242dee2f0ff62a
Luc M <luc.momal@gmail.com>
2016-04-23 14:52:58 +0200

    Fix lint

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 import articlizer from '../lib/index.js'
 import test from 'tape'
 
-test('awesome:test', t => {
+test('awesome:test', (t) => {
   const message = 'everything is awesome'
   t.equals(articlizer('awesome'), message, message)
   t.end()
